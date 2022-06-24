0bd752fb3846e6421941fe37350cd7cdd94ac3d6
Volkov <volkovai@nih.gov>
2022-06-24 09:57:18 -0400

    Fix codesign issue with Qt on MAC

--- make_dmg.sh
+++ make_dmg.sh
@@ -1,15 +1,24 @@
+APPNAME="RPE_Detection"
 VERSION="1.1.2"
 rm -rf build/
 rm -rf dist/
 pyinstaller --clean --noconfirm build-app-dir.spec
-rm -rf dist/RPE_Detection/
-hdiutil create "RPE_Detection-Darwin0.dmg" -format UDRW -ov -volname "RPE_Detection" -fs HFS+ -srcfolder "dist/" -attach
-ln -s /Applications /Volumes/RPE_Detection/Applications
-mkdir /Volumes/RPE_Detection/.background
-cp MacOS/DMGbackground.tif /Volumes/RPE_Detection/.background/background.tif
-osascript MacOS/DMGSetup.scpt RPE_Detection
-hdiutil detach /Volumes/RPE_Detection
-#python MacOS/licenseDMG.py RPE_Detection-Darwin0.dmg Help/License.txt
-hdiutil convert RPE_Detection-Darwin0.dmg -format UDZO -o "dist/RPE_Detection-${VERSION}-Darwin.dmg"
-rm RPE_Detection-Darwin0.dmg
-dmg-license MacOS/dmg-license-spec.json "dist/RPE_Detection-${VERSION}-Darwin.dmg"
+rm -rf "dist/${APPNAME}/"
+cd "dist/${APPNAME}.app/Contents/MacOS"
+mv PyQt5/ ../Resources/
+ln -s ../Resources/PyQt5 .
+cd ../Resources/
+ln -s ../MacOS/Qt* .
+cd ../../../..
+codesign --force --deep --verbose -s - "dist/${APPNAME}.app"
+codesign --verify --verbose --strict "dist/${APPNAME}.app"
+hdiutil create "${APPNAME}-Darwin0.dmg" -format UDRW -ov -volname "${APPNAME}" -fs HFS+ -srcfolder "dist/" -attach
+ln -s /Applications "/Volumes/${APPNAME}/Applications"
+mkdir "/Volumes/${APPNAME}/.background"
+cp MacOS/DMGbackground.tif "/Volumes/${APPNAME}/.background/background.tif"
+osascript MacOS/DMGSetup.scpt "${APPNAME}"
+hdiutil detach "/Volumes/${APPNAME}"
+#python MacOS/licenseDMG.py ${APPNAME}-Darwin0.dmg Help/License.txt
+hdiutil convert "${APPNAME}-Darwin0.dmg" -format UDZO -o "dist/${APPNAME}-${VERSION}-Darwin.dmg"
+rm "${APPNAME}-Darwin0.dmg"
+dmg-license MacOS/dmg-license-spec.json "dist/${APPNAME}-${VERSION}-Darwin.dmg"
